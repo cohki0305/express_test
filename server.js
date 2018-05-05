fbaf56d4651a4fa0d2e92ebc7dde4106e3defd55
cohki0305 <cohki0305@gmail.com>
2018-05-05 14:53:59 +0900

    use express

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>')
+
+  // return json
+  res.send({
+    name: 'Koki',
+    likes: [
+      'Netflix',
+      'Ruby'
+    ]
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  })
+})
+
+app.listen(3000)
