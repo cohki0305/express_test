1a6e74b4f98957a76b9bed18d103435cb6b79515
cohki0305 <cohki0305@gmail.com>
2018-05-05 15:37:48 +0900

    use middleware

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 
 const app = express()
 
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  const log = `${now}: ${req.method} ${req.url}`
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs')
+})
 
 hbs.registerPartials(__dirname + '/views/partials')
 
