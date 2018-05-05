062aa8b176192cd7bb2a4f55d2fab40b3bf17a5f
cohki0305 <cohki0305@gmail.com>
2018-05-05 15:14:18 +0900

    use hbs

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>')
-
-  // return json
-  res.send({
-    name: 'Koki',
-    likes: [
-      'Netflix',
-      'Ruby'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page'
   })
 })
 
 app.get('/about', (req, res) => {
-  res.send('About Page')
+  res.render('about.hbs', {
+    pageTitle: 'About Pages',
+    currentYear: new Date().getFullYear()
+  })
 })
 
 app.get('/bad', (req, res) => {
